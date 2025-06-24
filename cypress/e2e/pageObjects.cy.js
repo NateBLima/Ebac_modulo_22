4c20560ee8672bd5d9700c119a402f786c07b49f
Nathan <nathanbrlima@gmail.com>
2025-06-24 16:59:51 -0300

    aula 3-app actions

--- cypress/e2e/pageObjects.cy.js
+++ cypress/e2e/pageObjects.cy.js
@@ -1,20 +1,12 @@
 /// <reference types="cypress" />
-const {homePage} = require("../pages/home.page")
-const loginPage = require("../pages/login.page")
-const {email, password} = require("../fixtures/example.json")
+
+const { email, password } = require("../fixtures/example.json")
 import profilePage from '../pages/profile.page';
 
 describe('Teste de autenticação', () => {
-
-  beforeEach(() => {
-    cy.setCookie('EbacStoreVersion', 'V2')
-    cy.visit('/')
-  });
-
+  
   it('Deve fazer Login com Sucesso', () => {
-    homePage.openMenu('Account')
-    loginPage.login(email, password)
-    homePage.openMenu('Account')
+    cy.login(email, password)
     profilePage.customerName().should('contain', 'Lima')
   })
 })
