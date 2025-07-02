9de6ae9a89266535ab596c5f01ff0194236f9e1b
Nathan <nathanbrlima@gmail.com>
2025-07-02 14:06:16 -0300

    commit final - tarefa corrigida

--- cypress/support/appActions.js
+++ cypress/support/appActions.js
@@ -11,30 +11,29 @@ export const addItemToCart = () => {
     .scrollIntoView()
     .click();
 
- 
+
   cy.get('[data-testid="addToCart"] > .css-146c3p1').click();
 };
 
 export const proceedToCheckout = () => {
-  /* "Adicionar novo endereço" usado apenas uma vez, para cadastrar endereço
+  /* 
+  //"Adicionar novo endereço" usado apenas uma vez, para cadastrar endereço
   cy.get('[data-testid="addNewAddress"] > .r-lrvibr').click();
 
   // Preenche os dados do novo endereço
-  cy.get(':nth-child(1) > .css-175oi2r > .css-11aywtz').type('Nathan Brandão');     // Nome
-  cy.get(':nth-child(2) > .css-175oi2r > .css-11aywtz').type('31999999999');        // Celular
-  cy.get(':nth-child(3) > .css-175oi2r > .css-11aywtz').type('Rua dos Testes, 123'); // Endereço
-  cy.get(':nth-child(4) > .css-175oi2r > .css-11aywtz').type('Contagem');           // Cidade
-  cy.get(':nth-child(5) > .css-175oi2r > .css-11aywtz').type('MG');                 // Estado
-  cy.get(':nth-child(6) > .css-175oi2r > .css-11aywtz').type('32000000');           // CEP
+  cy.get(':nth-child(1) > .css-175oi2r > .css-11aywtz').type('Nathan Brandão');     
+  cy.get(':nth-child(2) > .css-175oi2r > .css-11aywtz').type('31999999999');        
+  cy.get(':nth-child(3) > .css-175oi2r > .css-11aywtz').type('Rua dos Testes, 123'); 
+  cy.get(':nth-child(4) > .css-175oi2r > .css-11aywtz').type('Contagem');           
+  cy.get(':nth-child(5) > .css-175oi2r > .css-11aywtz').type('MG');                
+  cy.get(':nth-child(6) > .css-175oi2r > .css-11aywtz').type('32000000');          
 
   // Salva o endereço
   cy.get('[data-testid="save"] > .css-146c3p1').click();
   */
 
-  // Clica duas vezes para continuar até pagamento
-cy.get('[data-testid="selectAddressOrContinueToPayment"] > .css-146c3p1').click();
-cy.get('[data-testid="completeCheckout"] > .css-146c3p1').click();
+  cy.get('[data-testid="selectAddressOrContinueToPayment"] > .css-146c3p1').click();
+  cy.get('[data-testid="completeCheckout"] > .css-146c3p1').click();
 
-  // Validação final
   cy.contains('Go Back Home').should('be.visible');
 };
