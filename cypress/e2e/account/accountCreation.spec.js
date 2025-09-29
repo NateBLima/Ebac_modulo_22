3acabe2dac9a357c6858af2c02333857345fb953
Nathan <nathanbrlima@gmail.com>
2025-09-29 16:57:01 -0300

    módulo 26 - C.I

--- cypress/e2e/account/accountCreation.spec.js
+++ cypress/e2e/account/accountCreation.spec.js
@@ -10,12 +10,11 @@ describe('Criação de Conta - Ecommerce', () => {
   });
 
   it('Deve criar uma conta com sucesso', () => {
-    // Gera massa de dados a cada execução
-    const email = faker.internet.email();
-    const password = faker.internet.password(10); // mínimo de 10 caracteres
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
-    const mobile = faker.phone.number('##9########');
+    const email = `${firstName}.${lastName}.${Date.now()}@ebac.com.br`.toLowerCase();
+    const password = faker.internet.password(10);
+    const mobile = faker.phone.number('55###########');
 
     accountPage.fillForm({
       firstName,
